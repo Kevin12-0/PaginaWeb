99de65f14e30f35c9e0ba8e52da9ce3d25a2e4f3
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-04-23 11:19:01 -0500

    registro de modelo a admin

--- Usuarios/roles/models.py
+++ Usuarios/roles/models.py
@@ -4,7 +4,15 @@ from django.db import models
 
 class usuarios (models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length= 200, blank= False, null=False)
-    apellido = models.CharField(max_length = 200, blank= False, null=False)
-    email = models.CharField(max_length = 200, blank= False, null=False)
-    contrasena = models.CharField(max_length = 200, blank= False, null=False)
+    nombre = models.CharField('Nombre(s)',max_length= 200, blank= False, null=False)
+    apellido = models.CharField('Apellidos',max_length = 200, blank= False, null=False)
+    email = models.CharField('Correo',max_length = 200, blank= False, null=False)
+    contrasena = models.CharField('Contraseña',max_length = 200, blank= False, null=False)
+
+    class Meta:
+        verbose_name = 'Usuarios'
+        verbose_name_plural = 'Usuario(s)'
+        ordering = ['nombre']
+    
+    def __str__(self):
+        return self.nombre
