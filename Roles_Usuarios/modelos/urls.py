9ff348710aa3363b561cd7219a151133363b89c4
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-04-25 23:40:52 -0500

    operaciones crud de usuario

--- Roles_Usuarios/modelos/urls.py
+++ Roles_Usuarios/modelos/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, registrarUsuario,listar_usuarios
+from .views import index, registrarUsuario,listar_usuarios,editar_usuario,eliminar_usuarios
 
 urlpatterns = [
     path("index/",index, name="Index"),
     path("registro-usuario/",registrarUsuario, name="registro"),
     path("listar-usuarios/",listar_usuarios, name="listar_usuarios"),
+    path("editar-usuario/<int:id>",editar_usuario, name="editar_usuario"),
+    path("eliminar-usuario/<int:id>",eliminar_usuarios, name="eliminar_usuarios")
 ]
--- Roles_Usuarios/modelos/views.py
+++ Roles_Usuarios/modelos/views.py
@@ -16,5 +16,24 @@ def registrarUsuario(request):
 def listar_usuarios(request):
     context = {'listar_usuarios': Usuarios.objects.all()}
     return render(request, 'usuarios/listado_usuario.html',context)
-    
-
+def editar_usuario(request,id=0): 
+    if request.method == 'GET':
+        if id == 0:
+            usuarios_form = usuarioForm()
+        else:
+            usuario = Usuarios.objects.get(pk=id)
+            usuarios_form = usuarioForm(instance=usuario)
+        return render (request, 'usuarios/registro_usuario.html',{'usuarios_form':usuarios_form})
+    else:
+        if id == 0:
+            usuarios_form = usuarioForm(request.POST)
+        else:
+            usuario = Usuarios.objects.get(pk=id)
+            usuarios_form = usuarioForm(request.POST,instance=usuario)
+        if usuarios_form.is_valid():
+            usuarios_form.save()
+    return redirect('listar_usuarios')
+def eliminar_usuarios(request, id):
+    usuario= Usuarios.objects.get(pk=id)
+    usuario.delete()
+    return redirect('listar_usuarios')
