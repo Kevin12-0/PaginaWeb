be41cdf96d644595e5f9097e4233b87830edd264
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-04-28 23:55:44 -0500

    url general.html

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
-     path('home/',home, name = 'home')
+     path('',home, name = 'home'),
+     path('generales/',generales, name = 'generales'),
 ]
