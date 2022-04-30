238cd703650135162d2ea790c90a3a430b5b7b57
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-04-30 01:43:57 -0500

    carga de datos por vista

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,17 +2,37 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 
+
 def home(request):
-    posts = Post.objects.filter(estado = True)
-    return render(request, 'index.html',{'posts':posts})
+    posts = Post.objects.filter(estado=True)
+    return render(request, 'index.html', {'posts': posts})
+
+
 def generales(request):
-    return render(request, 'generales.html')
+    posts = Post.objects.filter(
+        estado=True, categoria=Categoria.objects.get(nombre='Generales'))
+    return render(request, 'generales.html', {'posts': posts})
+
+
 def tecnologia(request):
-    return render(request, 'tecnologia.html')
+    posts = Post.objects.filter(
+        estado=True, categoria=Categoria.objects.get(nombre='Tecnologia'))
+    return render(request, 'tecnologia.html', {'posts': posts})
+
+
 def videojuegos(request):
-    return render(request, 'videojuegos.html')
+    posts = Post.objects.filter(
+        estado=True, categoria=Categoria.objects.get(nombre='Videojuegos'))
+    return render(request, 'videojuegos.html', {'posts': posts})
+
+
 def musica(request):
-    return render(request, 'musica.html') 
+    posts = Post.objects.filter(
+        estado=True, categoria=Categoria.objects.get(nombre='Musica'))
+    return render(request, 'musica.html', {'posts': posts})
+
+
 def tutoriales(request):
-    return render(request, 'tutoriales.html')  
-    
+    posts = Post.objects.filter(
+        estado=True, categoria=Categoria.objects.get(nombre='Tutoriales'))
+    return render(request, 'tutoriales.html', {'posts': posts})
