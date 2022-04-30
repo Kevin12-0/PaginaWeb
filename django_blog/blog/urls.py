c357aa47388acf1790557c161877bc5fe93d1283
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-04-30 12:37:44 -0500

    correccion de urls y de busqueda de archivos

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,10 +3,10 @@ from .views import *
 
 urlpatterns = [
      path('',home, name = 'home'),
-     path('<slug:slug>/',detallePost,name = 'detalle_post'),
      path('generales/',generales, name = 'generales'),
      path('tecnologia/',tecnologia, name = 'tecnologia'),
      path('videojuegos/',videojuegos,name='videojuegos'),
      path('musica/',musica,name = 'musica'),
-     path('tutoriales/',tutoriales,name='tutoriales')
+     path('tutoriales/',tutoriales,name='tutoriales'),
+     path('<slug:slug>/',detallePost,name = 'detalle_post'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 # Create your views here.
 
@@ -8,34 +8,34 @@ def home(request):
     return render(request, 'index.html', {'posts': posts})
 
 def detallePost(request,slug):
-    post = Post.objects.get(slug = slug)
+    post = get_object_or_404(Post,slug=slug)
     return render(request, 'post.html',{'detalle_post':post})
 
 def generales(request):
     posts = Post.objects.filter(
-        estado=True, categoria=Categoria.objects.get(nombre='Generales'))
+        estado=True, categoria=Categoria.objects.get(nombre__iexact = 'Generales'))
     return render(request, 'generales.html', {'posts': posts})
 
 
 def tecnologia(request):
     posts = Post.objects.filter(
-        estado=True, categoria=Categoria.objects.get(nombre='Tecnologia'))
+        estado=True, categoria=Categoria.objects.get(nombre__iexact='Tecnologia'))
     return render(request, 'tecnologia.html', {'posts': posts})
 
 
 def videojuegos(request):
     posts = Post.objects.filter(
-        estado=True, categoria=Categoria.objects.get(nombre='Videojuegos'))
+        estado=True, categoria=Categoria.objects.get(nombre__iexact='Videojuegos'))
     return render(request, 'videojuegos.html', {'posts': posts})
 
 
 def musica(request):
     posts = Post.objects.filter(
-        estado=True, categoria=Categoria.objects.get(nombre='Musica'))
+        estado=True, categoria=Categoria.objects.get(nombre__iexact='Musica'))
     return render(request, 'musica.html', {'posts': posts})
 
 
 def tutoriales(request):
     posts = Post.objects.filter(
-        estado=True, categoria=Categoria.objects.get(nombre='Tutoriales'))
+        estado=True, categoria=Categoria.objects.get(nombre__iexact='Tutoriales'))
     return render(request, 'tutoriales.html', {'posts': posts})
