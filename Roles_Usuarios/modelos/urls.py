7ed3cf36fb3e720ca3e3ecd031c9218cfcc8b7d1
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-05-01 01:30:55 -0500

    class editar usuario

--- Roles_Usuarios/modelos/urls.py
+++ Roles_Usuarios/modelos/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("",Inicio.as_view(), name="index"),
+    path("",Inicio.as_view(), name="Index"),
     path("registro-usuario/",registrarUsuario, name="registro"),
-    path("listar-usuarios/",listar_usuarios, name="listar_usuarios"),
-    path("editar-usuario/<int:id>",editar_usuario, name="editar_usuario"),
+    path("listar-usuarios/",ListadoUsuarios.as_view(), name="listar_usuarios"),
+    path("editar-usuario/<int:pk>",EditarUsuario.as_view(), name="editar_usuario"),
     path("eliminar-usuario/<int:id>",eliminar_usuarios, name="eliminar_usuarios")
 ]
--- Roles_Usuarios/modelos/views.py
+++ Roles_Usuarios/modelos/views.py
@@ -2,11 +2,24 @@ from django import dispatch
 from django.shortcuts import render, redirect
 from .forms import usuarioForm
 from .models import Usuarios
-from django.views.generic import View
+from django.views.generic import *
+from django.urls import reverse_lazy
 # Create your views here.
-class Inicio(View):
-    def get(self, request, *args, **kwargs):
-        return render(request,'index.html')
+class Inicio(TemplateView):
+    template_name = 'index.html'
+
+class ListadoUsuarios(ListView):
+    model = Usuarios
+    template_name = 'usuarios/listado_usuario.html'
+    context_object_name = 'listar_usuarios'
+    queryset = Usuarios.objects.all()
+
+class EditarUsuario(UpdateView):
+    model = Usuarios
+    template_name = 'usuarios/registro_usuario.html'
+    form_class  = usuarioForm
+    success_url = reverse_lazy('listar_usuarios')# nombre de la url
+
 def registrarUsuario(request):
     if request.method == 'POST':
         usuarios_form = usuarioForm(request.POST)
@@ -16,26 +29,7 @@ def registrarUsuario(request):
     else:
         usuarios_form = usuarioForm
     return render(request, 'usuarios/registro_usuario.html',{'usuarios_form':usuarios_form})
-def listar_usuarios(request):
-    context = {'listar_usuarios': Usuarios.objects.all()}
-    return render(request, 'usuarios/listado_usuario.html',context)
-def editar_usuario(request,id=0): 
-    if request.method == 'GET':
-        if id == 0:
-            usuarios_form = usuarioForm()
-        else:
-            usuario = Usuarios.objects.get(pk=id)
-            usuarios_form = usuarioForm(instance=usuario)
-        return render (request, 'usuarios/registro_usuario.html',{'usuarios_form':usuarios_form})
-    else:
-        if id == 0:
-            usuarios_form = usuarioForm(request.POST)
-        else:
-            usuario = Usuarios.objects.get(pk=id)
-            usuarios_form = usuarioForm(request.POST,instance=usuario)
-        if usuarios_form.is_valid():
-            usuarios_form.save()
-    return redirect('listar_usuarios')
+
 def eliminar_usuarios(request, id):
     usuario= Usuarios.objects.get(pk=id)
     usuario.delete()
