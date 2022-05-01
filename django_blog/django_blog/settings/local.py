7926d705f152f85d7d8c1f640e36d983914cafc6
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-04-30 20:53:47 -0500

    configuracion carpeta settings

---
+++ django_blog/django_blog/settings/local.py
@@ -0,0 +1,16 @@
+
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'Home',
+        'HOST': 'localhost'
+    }
+}
+
