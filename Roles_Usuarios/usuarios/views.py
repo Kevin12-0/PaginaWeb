bced6b247bd2d0b0566cda723670a3fb9cea8534
Kevin Lopez <1719110155tectulancingo.edu.mx>
2022-05-02 22:45:17 -0500

    cambios 0

--- Roles_Usuarios/usuarios/views.py
+++ Roles_Usuarios/usuarios/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-from django.views.generic.edit
-# Create your views here.
+from django.views.generic.edit import FormView
+from django.utils.decorators import *
+from django.views.decorators.cache import *
+from django.views.decorators.csrf import *
+from .forms import *
+from django.urls import *
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = FormularioLogin
+    success_url = reverse_lazy('index')
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -114,3 +114,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'Home',
+        'HOST': 'localhost'
+    }
+}
